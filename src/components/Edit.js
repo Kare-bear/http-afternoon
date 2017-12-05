e9e9d10d52d278fe24337cbb3155d6fab8a3ae9f
Kara Rainey <karain82@hotmail.com>
2017-12-05 17:14:01 -0600

    completed step 5

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import ConfirmModal from './subcomponents/ConfirmModal';
 
 // import axios
@@ -19,7 +20,7 @@ class Edit extends Component {
     }
 
     // insert componentWillMount
-
+    
     
     // insert updatePost 
     
