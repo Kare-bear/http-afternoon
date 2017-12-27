1058f81adbdd4e33abe319d6fcb30528b1997411
Kara Rainey <karain82@hotmail.com>
2017-12-26 18:37:59 -0600

    finished

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 //import axios
-import axios from 'axios'
+
 
 class NewUser extends Component{
     constructor(){
@@ -13,13 +15,29 @@ class NewUser extends Component{
         }
     }
     
-    // insert addUser
+    addUser(){
+        axios.post('/api/users', this.state).then(response=>{
+            console.log(response)
+            let user = response.data
+            this.props.history.push(`/user/${user.id}`)
+        })
+    }
 
-
-    // insert updateUser    
-
-
-    // insert deleteUser
+    updateUser(){
+        let id = this.props.match.params.id
+        axios.put(`/api/user/${id}`, this.state).then(response=>{
+            console.log(response)
+            let user = response.data
+            this.props.history.push(`/user/${user.id}`)
+        })
+    }
+    
+    deleteUser(){
+        let id = this.props.match.params.id
+        axios.delete(`/api/user/${id}`).then(response=>{
+            this.props.history.push(`/search`)
+        })
+    }
 
 
     render(){
